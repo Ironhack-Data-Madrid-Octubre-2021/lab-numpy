c195cf43701723d9db46a9b6bc8a7472e77ea380
Blanca Vispo <b.vispo@hotmail.com>
2021-10-26 18:41:20 +0200

    Lab-numpy terminado, todo ok

--- your-code/main.py
+++ your-code/main.py
@@ -1,85 +1,124 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
+from numpy.core.shape_base import atleast_3d
 
 #2. Print the NUMPY version and the configuration.
 
-
+#print(np.__version__)
+#print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random(size = (2,3,5))
 
+a_2 = np.random.randint(1,10, size = (2,3,5))
+
+a_3 = np.random.sample(size=(2,3,5))
 
 #4. Print a.
 
-
+#print(a)
+#print(a_2)
+#prrint(a_3)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.random.randint(1,2, size=(5,2,3))
 
 #6. Print b.
 
+#print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+#print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#print(a.shape == b.shape)
+#as the shapes of both arrays are different, we cannot add them.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+b.transpose(1,2,0)
 
+c = b.transpose(1,2,0)
+#print(c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
+#print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+#print(a)
+#print(d)
+#The values on array d are the sum of the values of the array 1 + b_traspose, which is made off only 1s.
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
+#print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
+#print(e == a)
+#Yes, they equal as the result is True: this is because c is an arrray made of 1s.
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
-
+#print(d_max)
+#print(d_min)
+#print(d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty([2,3,5])
+#print(f)
 
 
-
-"""
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
-If a value equals to d_mean, assign 50 to the corresponding value in f.
-Assign 0 to the corresponding value(s) in f for d_min in d.
-Assign 100 to the corresponding value(s) in f for d_max in d.
-In the end, f should have only the following values: 0, 25, 50, 75, and 100.
-Note: you don't have to use Numpy in this question.
-"""
+#If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+#If a value equals to d_mean, assign 50 to the corresponding value in f.
+#Assign 0 to the corresponding value(s) in f for d_min in d.
+#Assign 100 to the corresponding value(s) in f for d_max in d.
+#In the end, f should have only the following values: 0, 25, 50, 75, and 100.
+#Note: you don't have to use Numpy in this question.
+
+for x in range(d.shape[0]):
+        for y in range(d.shape[1]):
+                for z in range(d.shape[2]):
+                        if d[x][y][z] > d_min and d[x][y][z] < d_mean:
+                                f[x][y][z] = 25     
+                        elif d[x][y][z] > d_mean and d[x][y][z] < d_max:
+                                f[x][y][z] = 75    
+                        elif d[x][y][z] == d_mean:
+                                f[x][y][z] = 50     
+                        elif (d_min) == d[x][y][z]:
+                                f[x][y][z] = 0
+                        elif (d_max) == d[x][y][z]:
+                                f[x][y][z] = 100
+
+#print(f)
 
 
-
-
-"""
 #17. Print d and f. Do you have your expected f?
+#print(d)
+#print(f)
+#Yes.
+
+""""
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
@@ -112,3 +151,19 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+f = np.ndarray.astype(f, dtype = str)
+for x in range(d.shape[0]):
+        for y in range(d.shape[1]):
+                for z in range(d.shape[2]):
+                        if d[x][y][z] > d_min and d[x][y][z] < d_mean:
+                                f[x][y][z] = "B"     
+                        elif d[x][y][z] > d_mean and d[x][y][z] < d_max:
+                                f[x][y][z] = "D"    
+                        elif d[x][y][z] == d_mean:
+                                f[x][y][z] = "C"     
+                        elif (d_min) == d[x][y][z]:
+                                f[x][y][z] = "A"
+                        elif (d_max) == d[x][y][z]:
+                                f[x][y][z] = "E"
+
+#print(f)
