35694a75c999d317afea562d30466432987aba8d
pedromartinezleis <60599980+pedromartinezleis@users.noreply.github.com>
2021-10-26 18:42:03 +0200

    Add files via upload

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,73 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+#print(np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+a = np.random.random((2,3,5))
 
 
 
 #4. Print a.
-
+#print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,2,3))
 
 
 #6. Print b.
-
+#print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
-
+#if a.size == b.size:
+ #       print("Igual")
+#else:
+ #       print("No igual")
 
 #8. Are you able to add a and b? Why or why not?
-
+#No se puede pq tienen formas diferentes [operands could not be broadcast together with shapes (2,3,5) (5,2,3)]
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.transpose(1,2,0)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d = np.add(a, c)
+#ahora funciona pq tienen la misma forma
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
+#print(a)
+#print(d)
+#son el mismo array solo que d tiene sus valores incrementados en 1
 
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a * c
+#print(e)
 
 #13. Does e equal to a? Why or why not?
-
+#son iguales porque c es una matriz de 1
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2,3,5))
+#print(f)
 
 
 
@@ -73,7 +79,32 @@ Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
-"""
+if value == d_min : f = 0
+if value > d_min and value < d_mean : f = 25
+if value == de_mean : f=50
+if value > d_mean and value < d_max: f = 75
+if value == d_max : f = 100
+#""
+np.shape(f)
+x = np.shape(d)[0]
+y = np.shape(d)[1]
+z = np.shape(d)[2]
+for i in range(x):
+        for t in range(y):
+                for u in range(z):
+                        if d[i][t][u] == d_min:
+                                f[i][t][u] = 0
+                        elif d[i][t][u] > d_min and  d[i][t][u] < d_mean:
+                                f[i][t][u] = 25
+                        elif d[i][t][u] == d_mean:
+                                f[i][t][u] = 50
+                        elif d[i][t][u] > d_mean and d[i][t][u] < d_max:
+                                f[i][t][u] = 75
+                        elif d[i][t][u] == d_max:
+                                f[i][t][u] = 100
+print(f)
+
+"""                          
 
 
 
@@ -112,3 +143,19 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+f = np.full([2,3,5],"")
+for i in range(len(d)):
+    for j in range(len(d[i])):
+        for k in range(len(d[i][j])):
+            if d[i][j][k] > d_min and d[i][j][k] < d_mean:
+                f[i][j][k] = "B"
+            elif d[i][j][k] > d_mean and d[i][j][k] < d_max:
+                f[i][j][k] = "D"
+            elif d[i][j][k] == d_mean:
+                f[i][j][k] = "C"
+            elif d[i][j][k] == d_max:
+                f[i][j][k] = "E"
+            else:
+                f[i][j][k] = "A"
+print(f)
