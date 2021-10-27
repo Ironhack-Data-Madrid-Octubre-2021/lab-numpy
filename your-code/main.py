c6397a9d6b3bea7ea64c0b1d898e511ccb427863
LuisSerranoCerame <serranocerameluis@gmail.com>
2021-10-27 08:54:04 +0200

    Por revisar

--- your-code/main.py
+++ your-code/main.py
@@ -1,6 +1,6 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
@@ -9,61 +9,67 @@
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+### a and b do not have the same size, a is 2x3x5 while b is 5x2x3
+print(len(a))
+print(len(b))
 
 #8. Are you able to add a and b? Why or why not?
 
-
+###We are not able to add a and b. Both matrices have diferent number of rows and columns. We will need to transpose on of them in order to carry the sum.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = np.transpose(b,(1,2,0))
+print(c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add(a,c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
 
-
+#### d matrix is equal to the a matrix +1 to each element.
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a*c
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
-
+####Yes because we have multiplied by value 1
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = d.min()
+d_min = d.max()
+d_mean = d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty([2,3,5])
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,9 +80,24 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
-
+np.shape(f)
+x = np.shape(d)[0]
+y = np.shape(d)[1]
+z = np.shape(d)[2]
+for i in range(x):
+    for t in range(y):
+        for u in range(z):
+            if d[i][t][u] == d_min:
+                f[i][t][u] = 0
+            elif d[i][t][u] > d_min and d[i][t][u] < d_mean:
+                f[i][t][u] = 25
+            elif d[i][t][u] == d_mean:
+                f[i][t][u] = 50
+            elif d[i][t][u] > d_mean and d[i][t][u] < d_max:
+                f[i][t][u] = 75
+            elif d[i][t][u] == d_max:
+                f[i][t][u] = 100
+print(f)
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -98,7 +119,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(d)
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +134,18 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+fff = np.full([2,3,5],"")
+for i in range(len(d)):
+    for j in range(len(d[i])):
+         for k in range(len(d[i][j])):
+                if d[i][j][k] > d_min and d[i][j][k] < d_mean:
+                    fff[i][j][k] = "B"
+                elif d[i][j][k] > d_mean and d[i][j][k] < d_max:
+                    fff[i][j][k] = "D"
+                elif d[i][j][k] == d_mean:
+                    fff[i][j][k] = "C"
+                elif d[i][j][k] == d_max:
+                    fff[i][j][k] = "E"
+                else:
+                    fff[i][j][k] = "A"
+print(fff)
